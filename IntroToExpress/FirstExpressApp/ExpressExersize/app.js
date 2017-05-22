238ad506bf856a4d4621a70c959e7e1ea872a069
raydabbah <dabbahray@yahoo.com>
2017-05-22 12:07:55 -0400

    Changed function animalSound to var and added an if else to remove errors

--- IntroToExpress/FirstExpressApp/ExpressExersize/app.js
+++ IntroToExpress/FirstExpressApp/ExpressExersize/app.js
@@ -1,31 +1,25 @@
 var express = require('express');
 var app = express();
-
-
 app.get("/", function(req, res) {
     res.send("Hello there!! Welcome to my glorious app!!");
 });
-
 app.get("/speak/:animal", function(req, res) {
     var animal = req.params.animal;
-   
-    function animalSound(a) {
-        if (a === 'pig') {
-        return 'OINK';
-    }   if (a === 'dog') {
-        return  'Ruff ruff';
-    }   if (a === 'horse') {
-        return 'Neigh';
-    } else {
+      var animalSound = ''; 
+        if (animal === 'pig') {
+        animalSound = 'OINK';
+    }   if (animal === 'dog') {
+       animalSound =  'Ruff ruff';
+    }   if (animal === 'horse') {
+        animalSound = 'Neigh';
+    } else if (animalSound === '') {
         res.send(`Sorry; we don't like ${animal.toUpperCase()}S! Please choose one that we do!!`);
     }
+    
+        if (animalSound !== '') {
+    res.send(`The ${animal} says ${animalSound}!`);    
     }
-   
-    res.send(`The ${animal} says ${animalSound(animal)}!`);    
-    
-    
 });
-
 app.get("/repeat/:hello/:numbers", function(req, res) {
    var hello = req.params.hello;
    var hi = '';
@@ -39,11 +33,9 @@ app.get("/repeat/:hello/:numbers", function(req, res) {
   console.log(req.params);
    res.send(hi);
 });
-
 app.get("*", function(req, res) {
     res.send("Sorry; this page doesn't exist");
     });
-
 app.listen(3000, 'localhost', function () {
     console.log('Server has started');
     });
