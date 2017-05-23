79c6226d826ea8eafb07677ca9e7fc68235ccd31
raydabbah <dabbahray@yahoo.com>
2017-05-23 13:01:24 -0400

    added foodTaste and now it's working with an if/else and a forEach.

--- Ejs/ejs.js
+++ Ejs/ejs.js
@@ -8,5 +8,20 @@ app.get("/eatapples/:thing", function(req, res) {
     var thing = req.params.thing;
     res.render("eatApples.ejs", {thingVar: thing.toLowerCase()});
 });
+app.get("/posts", function(req, res) {
+    var posts = [
+                 { title: 'Challah', taste: `Delicious!`},
+                  {title: `Bagels` , taste: `Great!`},
+                  {title: `Pita Bread` , taste: `Awesome!`},
+                  {title: `Rolls` , taste: `Not Bad.`},
+                  {title: `Whole wheat bread`, taste: `Michelle Obama...`}                   
+                 ];
+    res.render("foodTaste.ejs", {posts: posts});
+});
+app.get("*", function(req, res) {
+   
+    res.send(`You have reached a non-existant page.
+         Sorry; Hope yu have a better day tomorrow.`);
+});
 
 app.listen(3000, 'localhost', () => console.log('Server has started.'));
