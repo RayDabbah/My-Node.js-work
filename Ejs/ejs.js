8dba6d6dc78724d7f2bc415887fd8c14591a0e75
raydabbah <dabbahray@yahoo.com>
2017-05-23 12:05:33 -0400

    added some logic to eatApples.ejs

--- Ejs/ejs.js
+++ Ejs/ejs.js
@@ -6,7 +6,7 @@ app.get("/", function(req, res) {
     });
 app.get("/eatapples/:thing", function(req, res) {
     var thing = req.params.thing;
-    res.render("eatApples.ejs", {thingVar: thing});
+    res.render("eatApples.ejs", {thingVar: thing.toLowerCase()});
 });
 
 app.listen(3000, 'localhost', () => console.log('Server has started.'));
