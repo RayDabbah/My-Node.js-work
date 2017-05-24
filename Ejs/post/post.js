6080ef253b4cee994b7d989c9f458f59c21dc096
raydabbah <dabbahray@yahoo.com>
2017-05-24 13:42:54 -0400

    1st post request

---
+++ Ejs/post/post.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+var friends = ["Moshe", "Yankel", "Mottel", "Shmerel", "Yoeli", "Eli Baruch"];
+
+app.get('/', function(req, res){
+	res.render("friends.ejs", {friends: friends});
+});
+app.post('/addfriend', (req, res) => {
+	var newFriend = req.body.newFriend;
+	friends.push(newFriend);
+	console.log((newFriend));
+	res.send(`You have reached the post a new friend page.
+			 His name is ${newFriend}`);
+});
+
+app.listen(3000, 'localhost', ()=> console.log('The server has started.'));
