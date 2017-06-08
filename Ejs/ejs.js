e8a849f792935b63492336753254c8afdc7abe3b
raydabbah <dabbahray@yahoo.com>
2017-06-08 12:24:59 -0400

    beautified

--- Ejs/ejs.js
+++ Ejs/ejs.js
@@ -3,25 +3,43 @@ app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.get("/", function(req, res) {
-    res.render("home");
-    });
+	res.render("home");
+});
 app.get("/eatapples/:thing", function(req, res) {
-    var thing = req.params.thing;
-    res.render("eatApples", {thingVar: thing.toLowerCase()});
+	var thing = req.params.thing;
+	res.render("eatApples", {
+		thingVar: thing.toLowerCase()
+	});
 });
 app.get("/posts", function(req, res) {
-    var posts = [
-                 { title: 'Challah', taste: `Delicious!`},
-                  {title: `Bagels` , taste: `Great!`},
-                  {title: `Pita Bread` , taste: `Awesome!`},
-                  {title: `Rolls` , taste: `Not Bad.`},
-                  {title: `Whole wheat bread`, taste: `Michelle Obama...`}                   
-                 ];
-    res.render("foodTaste", {posts: posts});
+	var posts = [{
+			title: 'Challah',
+			taste: `Delicious!`
+		},
+		{
+			title: `Bagels`,
+			taste: `Great!`
+		},
+		{
+			title: `Pita Bread`,
+			taste: `Awesome!`
+		},
+		{
+			title: `Rolls`,
+			taste: `Not Bad.`
+		},
+		{
+			title: `Whole wheat bread`,
+			taste: `Michelle Obama...`
+		}
+	];
+	res.render("foodTaste", {
+		posts: posts
+	});
 });
 app.get("*", function(req, res) {
-   
-    res.send(`You have reached a non-existant page.
+
+	res.send(`You have reached a non-existant page.
          Sorry; Hope yu have a better day tomorrow.`);
 });
 
--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -1,32 +1,33 @@
 var express = require('express');
 var app = express();
 
-// "/" => "Hi There"
-app.get("/", function(request, response) {
-response.send('HI THERE');
-console.log(request);
-console.log('With params: ' + request.params);
-});
+	app.get("/", function(request, response) {
+		response.send('HI THERE');
+		//console.log(request);
+		console.dir(request.params);
+	});
 app.get('/goodbye', function(request, response) {
-    console.log("SOMEONE MADE A REQUEST TO Goodbye.");
-    response.send('Have a great day!!!!');
+	console.log("SOMEONE MADE A REQUEST TO Goodbye.");
+	response.send('Have a great day!!!!');
 });
 app.get('/g/:name/food/:favFood/game/:favGame', function(request, response) {
-    console.log(request.params);
-    var nameItem = request.params.name.toUpperCase();
-    var favFood = request.params.favFood;
-    var favGame = request.params.favGame;
-    response.send(`How are you ${nameItem}? \n So nice to see you here!!!
+	console.log(request.params);
+	var nameItem = request.params.name.toUpperCase();
+	var favFood = request.params.favFood;
+	var favGame = request.params.favGame;
+	console.dir(request.params);
+	console.log(request.params);
+	response.send(`How are you ${nameItem}? \n So nice to see you here!!!
 We would like to offer you some of your favorite ${favFood} while you play ${favGame}!!!`);
 });
 app.get('/doggies', function(request, response) {
-    console.log("SOMEONE MADE A REQUEST TO /DOGGIES!!!");
-    response.send('Dogs say Ruff Rufff!!!!!!!!!!!!!!!!!!!!!');
+	console.log("SOMEONE MADE A REQUEST TO /DOGGIES!!!");
+	response.send('Dogs say Ruff Rufff!!!!!!!!!!!!!!!!!!!!!');
 });
 app.get("*", function(request, response) {
-    console.log("SOMEONE MADE A REQUEST THAT MAKES NO SENSE!!!");
-response.send('We are very sorry; the page that you have requested is not in existance.');    
+	console.log("SOMEONE MADE A REQUEST THAT MAKES NO SENSE!!!");
+	response.send('We are very sorry; the page that you have requested is not in existance.');
 });
-app.listen(3000, 'localhost', function () {
-    console.log('Server has started');
-    });
+app.listen(3000, 'localhost', function() {
+	console.log('Server has started');
+});
